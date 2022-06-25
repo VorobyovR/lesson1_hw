3a46ed2b5125e9e0a30c0f329de1167bafe90321
VorobyovR <vorobyovromanmail@gmail.com>
2022-06-25 20:29:04 +0300

    2 task

--- hw_1/Program.cs
+++ hw_1/Program.cs
@@ -4,15 +4,41 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+// Console.Write("Input first number: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if (number1 > number2)
+// {
+//     Console.WriteLine("a = " + number1 + "; b = " + number2 + " -> " + "max = " + number1);
+// }
+// else 
+// {
+//     Console.WriteLine("a = " + number1 + "; b = " + number2 + " -> " + "max = " + number2);
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
 Console.Write("Input first number: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input second number: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
 if (number1 > number2)
 {
-    Console.WriteLine("a = " + number1 + "; b = " + number2 + " -> " + "max = " + number1);
+    if (number1 > number3)
+    Console.WriteLine(number1 + ", " + number2 + ", " + number3 + " -> " + number1);
 }
-else 
+else
 {
-    Console.WriteLine("a = " + number1 + "; b = " + number2 + " -> " + "max = " + number2);
+    if (number2 > number3)
+    Console.WriteLine(number1 + ", " + number2 + ", " + number3 + " -> " + number2);
+    else
+    Console.WriteLine(number1 + ", " + number2 + ", " + number3 + " -> " + number3);
 }
